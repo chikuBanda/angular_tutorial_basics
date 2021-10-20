032ba21c1fca92989e0404e008198ab07b5b4590
chikuBanda <chikubanda.cb@gmail.com>
2021-10-20 11:00:16 +0100

    basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'],
+  styles: [`
+    h3{
+      color: hotpink
+    }
+  `]
 })
 export class AppComponent {
-  title = 'my-first-app';
+  name = 'chiku banda';
 }
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ServerComponent } from '../server/server.component';
+
+@Component({
+  selector: 'app-servers',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
